85c76a90e36e3421dd3ed113cb873b7d25319ba7
Ke Tan <JupiterEthan@users.noreply.github.com>
2020-09-07 22:17:13 -0400

    Add files via upload

--- js/crossfade.js
+++ js/crossfade.js
@@ -1,27 +1,23 @@
-var CrossfadeSample = function() {
-  loadSounds(this, {
-    spkr1: 'src/crossfade/48_sph1_mimo_sagrnn.wav',
-    spkr2: 'src/crossfade/48_sph2_mimo_sagrnn.wav'
-  });
-  this.isPlaying = false;
-}
+var CrossfadeSample = {playing:false};
 
-CrossfadeSample.prototype.play = function() {
+CrossfadeSample.play = function() {
   // Create two sources.
-  this.ctl1 = createSource(this.spkr1);
-  this.ctl2 = createSource(this.spkr2);
+  this.ctl1 = createSource(BUFFERS.drums);
+  this.ctl2 = createSource(BUFFERS.organ);
   // Mute the second source.
   this.ctl1.gainNode.gain.value = 0;
   // Start playback in a loop
-  var onName = this.ctl1.source.start ? 'start' : 'noteOn';
-  this.ctl1.source[onName](0);
-  this.ctl2.source[onName](0);
-  // Set the initial crossfade to be just source 1.
-  this.crossfade(0);
+  if (!this.ctl1.source.start) {
+    this.ctl1.source.noteOn(0);
+    this.ctl2.source.noteOn(0);
+  } else {
+    this.ctl1.source.start(0);
+    this.ctl2.source.start(0);
+  }
 
   function createSource(buffer) {
     var source = context.createBufferSource();
-    var gainNode = context.createGain();
+    var gainNode = context.createGain ? context.createGain() : context.createGainNode();
     source.buffer = buffer;
     // Turn on looping
     source.loop = true;
@@ -37,14 +33,18 @@ CrossfadeSample.prototype.play = function() {
   }
 };
 
-CrossfadeSample.prototype.stop = function() {
-  var offName = this.ctl1.source.stop ? 'stop' : 'noteOff';
-  this.ctl1.source[offName](0);
-  this.ctl2.source[offName](0);
+CrossfadeSample.stop = function() {
+  if (!this.ctl1.source.stop) {
+    this.ctl1.source.noteOff(0);
+    this.ctl2.source.noteOff(0);
+  } else {
+    this.ctl1.source.stop(0);
+    this.ctl2.source.stop(0);
+  }
 };
 
 // Fades between 0 (all source 1) and 1 (all source 2)
-CrossfadeSample.prototype.crossfade = function(element) {
+CrossfadeSample.crossfade = function(element) {
   var x = parseInt(element.value) / parseInt(element.max);
   // Use an equal-power crossfading curve:
   var gain1 = Math.cos(x * 0.5*Math.PI);
@@ -53,7 +53,7 @@ CrossfadeSample.prototype.crossfade = function(element) {
   this.ctl2.gainNode.gain.value = gain2;
 };
 
-CrossfadeSample.prototype.toggle = function() {
-  this.isPlaying ? this.stop() : this.play();
-  this.isPlaying = !this.isPlaying;
+CrossfadeSample.toggle = function() {
+  this.playing ? this.stop() : this.play();
+  this.playing = !this.playing;
 };
